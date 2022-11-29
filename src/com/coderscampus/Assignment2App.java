bd32dd81599bb6da5f243cb5a6f59f5769ffd046
Francisco Sanchez <sanchezguevara.frank@gmail.com>
2022-11-29 11:56:44 -0500

    Updated version

--- src/com/coderscampus/Assignment2App.java
+++ src/com/coderscampus/Assignment2App.java
@@ -19,23 +19,27 @@ public class Assignment2App {
 	System.out.println("Guess a number between 1 and 100. You will have 5 tries.");
 		guess =scanner.nextInt();
 	
-	for (int i = 1 ; i < 6 ; i++) {
+	for (int i = 1 ; i < 5 ; i++) {
 	if (guess == answer) {
 		break;
+	} else if (guess > 100 | guess < 1) {
+		System.out.println("Please enter a number between 1 and 100");
+		guess = scanner.nextInt();
+		i = i-1;
 	} else if (guess > answer) {
-		System.out.println("Too high");
+		System.out.println("Too high. Try again");
 		guess = scanner.nextInt();
 	} else if (guess < answer) { 
-		System.out.println("Too low");
+		System.out.println("Too low. Try again");
 		guess = scanner.nextInt();
 		
-	}
-	}
+	    }
+}
 	if (guess == answer) {
 		System.out.println("You win!");
 	} else {
 		System.out.println("You lose.");
 	}
-
+	System.out.println("The correct number was " + answer);
 }
 }
